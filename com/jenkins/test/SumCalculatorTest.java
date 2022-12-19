fb096ac52708c9c06677c7e65910a7efb758a8e1
Dvd134 <102422049+Dvd134@users.noreply.github.com>
2022-12-19 18:36:39 +0200

    Add files via upload

---
+++ com/jenkins/test/SumCalculatorTest.java
@@ -0,0 +1,30 @@
+package com.jenkins.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumCalculatorTest {
+
+    SumCalcumator sumCalculator;
+    @Before
+    public void setUp() {
+
+        sumCalculator = new SumCalcumator();
+    }
+
+    @Test
+    public void testCorrectSum() {
+
+        int result = sumCalculator.getSumOfNumbers(1, 10);
+        int expectedResult = 55;
+        assertEquals(expectedResult, result);
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void testIncorrectArgumentsSum(){
+
+        int result = sumCalculator.getSumOfNumbers(10, 1);
+    }
+}
---
+++ com/jenkins/test/SumCalcumator.java
@@ -0,0 +1,17 @@
+package com.jenkins.test;
+
+public class SumCalcumator {
+
+    public int getSumOfNumbers(int from, int to) {
+
+        if(from > to)
+            throw new ArithmeticException();
+
+        int sum = 0;
+        for(int i = from ; i <= to ; i++) {
+
+            sum += i;
+        }
+        return sum;
+    }
+}

c0437f36dd761cefdc4c12bf6224f0e3a62fd186
Dvd134 <102422049+Dvd134@users.noreply.github.com>
2022-12-19 19:30:06 +0200

    Add files via upload

---
+++ SumCalcumator.java
@@ -0,0 +1,15 @@
+public class SumCalcumator {
+
+    public int getSumOfNumbers(int from, int to) {
+
+        if(from > to)
+            throw new ArithmeticException();
+
+        int sum = 0;
+        for(int i = from ; i <= to ; i++) {
+
+            sum += i;
+        }
+        return sum;
+    }
+}
